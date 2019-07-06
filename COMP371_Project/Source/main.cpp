48f8947d577796547016c4f09f93a346c8b17aa0
corentinartaud <corentinartaud@gmail.com>
2019-07-06 16:55:22 -0400

    Project Setup

---
+++ COMP371_Project/Source/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP371_Project
+//
+//  Created by Corentin Artaud on 2019-07-06.
+//  Copyright © 2019 Corentin Artaud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
