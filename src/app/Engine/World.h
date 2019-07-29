0994b3a40e1680a4d03a3f7884f92ee2b4c0c4d1
corentinartaud <corentinartaud@gmail.com>
2019-07-29 15:30:53 -0400

    Restructured

---
+++ src/app/Engine/World.h
@@ -0,0 +1,46 @@
+//
+// COMP 371 Assignment Framework
+//
+// Created by Nicolas Bergeron on 8/7/14.
+// Updated by Gary Chang on 14/1/15
+//
+// Copyright (c) 2014-2019 Concordia University. All rights reserved.
+//
+
+#pragma once
+
+
+#include "ParsingHelper.h"
+#include <vector>
+
+class Camera;
+class Model;
+class Animation;
+class AnimationKey;
+
+class World
+{
+public:
+	World();
+	~World();
+	
+    static World* GetInstance();
+
+	void Update(float dt);
+	void Draw();
+
+	void LoadScene(const char * scene_path);
+    Animation* FindAnimation(ci_string animName);
+    AnimationKey* FindAnimationKey(ci_string keyName);
+
+    const Camera* GetCurrentCamera() const;
+    
+private:
+    static World* instance;
+    
+	std::vector<Model*> mModel;
+    std::vector<Animation*> mAnimation;
+    std::vector<AnimationKey*> mAnimationKey;
+	std::vector<Camera*> mCamera;
+	unsigned int mCurrentCamera;
+};
