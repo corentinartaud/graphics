2f2e8793b94ab59274b1332cec633971e396cceb
Yashar <yashardabiran75@gmail.com>
2019-08-09 23:52:04 -0400

    Added camera movement + map update

--- Source/Resources/Renderer.h
+++ Source/Resources/Renderer.h
@@ -24,7 +24,7 @@ public:
     // Destructor
     ~Renderer();
     // Renders a defined quad textured with given sprite
-    void Render(TextureLoader &texture, glm::vec2 position, glm::vec2 size = glm::vec2(10, 10), GLfloat rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f), glm::vec2 textureScaling = glm::vec2(1.f, 1.f));
+    void Render(TextureLoader &texture, glm::vec2 position, glm::vec2 size = glm::vec2(10, 10), GLfloat rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f), glm::vec2 textureScaling = glm::vec2(1.f, 1.f), glm::mat4 viewMatrix = glm::mat4(1.f));
 private:
     // Render state
     Shader shader;
