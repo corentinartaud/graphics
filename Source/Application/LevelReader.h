e10cf22f0ad4e3b205eadf232c64408130da83ef
Yashar <yashardabiran75@gmail.com>
2019-08-11 13:54:26 -0400

    Fixed spike scaling

--- Source/Application/LevelReader.h
+++ Source/Application/LevelReader.h
@@ -26,6 +26,8 @@ private:
     static GameObject* ReadPlayer(std::ifstream&);
     static GameObject ReadGround(std::ifstream&);
     static void ReadObjects(std::ifstream&, std::vector<GameObject>&, TextureLoader texture);
+    // Adds spike individuals instead of spike groups defined with "scale: X.0 1.0" (X > 1)
+    static void PopulateSpikes(std::vector<GameObject>&);
 };
 
 #endif /* LevelReader_h */
