74bb6ff496fb8b391dcf45fac96010045576e9ca
Yashar <yashardabiran75@gmail.com>
2019-08-10 21:11:51 -0400

    Added triangle rendering to Renderer

--- Source/Resources/Renderer.h
+++ Source/Resources/Renderer.h
@@ -24,15 +24,18 @@ public:
     // Destructor
     ~Renderer();
     // Renders a defined quad textured with given sprite
-    void Render(TextureLoader &texture, glm::vec2 position, glm::vec2 size = glm::vec2(10, 10), GLfloat rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f), glm::vec2 textureScaling = glm::vec2(1.f, 1.f), glm::mat4 viewMatrix = glm::mat4(1.f));
+    void Render(TextureLoader &texture, glm::vec2 position, glm::vec2 size = glm::vec2(10, 10), GLfloat rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f), glm::vec2 textureScaling = glm::vec2(1.f, 1.f), glm::mat4 viewMatrix = glm::mat4(1.f), bool quad = true);
 private:
     // Render state
     Shader shader;
     // Initializes and configures the quad's buffer and vertex attributes
     void configureQuad();
     void renderQuad();
+    void configureTriangle();
+    void renderTriangle();
     
-    unsigned int mVAO;
+    unsigned int mQuadVAO;
+    unsigned int mTriangleVAO;
     unsigned int mVBO;
 };
 
